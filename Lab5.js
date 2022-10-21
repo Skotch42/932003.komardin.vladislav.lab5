5d7b8a5809253acddbe65716b2ae0ac08343fd3d
Комардин Влад <99380380+Skotch42@users.noreply.github.com>
2022-10-21 11:39:20 +0700

    Add files via upload

---
+++ Lab5.js
@@ -0,0 +1,23 @@
+function button1() {
+	document.getElementsByClassName("back")[0].style.display = 'block';
+	document.getElementById('news1').style.opacity = '0.3';
+	document.getElementById('news2').style.opacity = '0.3';
+	document.getElementById('news3').style.opacity = '0.3';
+	document.getElementById('description1').style.display = 'block';
+}
+
+function button2() {
+	document.getElementsByClassName("back")[0].style.display = 'block';
+	document.getElementById('news1').style.opacity = '0.3';
+	document.getElementById('news2').style.opacity = '0.3';
+	document.getElementById('news3').style.opacity = '0.3';
+	document.getElementById('description2').style.display = 'block';
+}
+
+function button3() {
+	document.getElementsByClassName("back")[0].style.display = 'block';
+	document.getElementById('news1').style.opacity = '0.3';
+	document.getElementById('news2').style.opacity = '0.3';
+	document.getElementById('news3').style.opacity = '0.3';
+	document.getElementById('description3').style.display = 'block';
+}
