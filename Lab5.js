c6f8b211871e6cb3ac7b5623f52a52c69cd6de9d
Комардин Влад <99380380+Skotch42@users.noreply.github.com>
2022-11-25 01:26:54 +0700

    Update Lab5.js

--- Lab5.js
+++ Lab5.js
@@ -21,3 +21,13 @@ function button3() {
 	document.getElementById('news3').style.opacity = '0.3';
 	document.getElementById('description3').style.display = 'block';
 }
+
+function close_all() {
+	document.getElementsByClassName("back")[0].style.display ="none";
+    document.getElementById('description1').style.display = 'none';
+	document.getElementById('description2').style.display = 'none';
+	document.getElementById('description3').style.display = 'none';
+	document.getElementById('news1').style.opacity = '1';
+	document.getElementById('news2').style.opacity = '1';
+	document.getElementById('news3').style.opacity = '1';
+}
